39ded6317d3f9ddf5f6f145a808469196d8747ea
Arkiver2 <arkiver@protonmail.com>
2018-07-29 01:23:54 +0200

    Ignore two more URLs. Bump version to 20180729.03.

--- pipeline.py
+++ pipeline.py
@@ -63,7 +63,7 @@ if not WGET_LUA:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20180729.02'
+VERSION = '20180729.03'
 USER_AGENT = 'ArchiveTeam'
 TRACKER_ID = 'tindeck'
 TRACKER_HOST = 'tracker.archiveteam.org'
